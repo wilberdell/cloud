c2771e13a520e085901795d6a8eab1bb185be4df
wilberdell <54139296+wilberdell@users.noreply.github.com>
2019-08-16 17:46:32 -0500

    Update setup_vm2.sh

--- azure_script/setup_vm2.sh
+++ azure_script/setup_vm2.sh
@@ -2,9 +2,9 @@
 sudo apt-get -y update       
 sudo apt-get -y upgrade  
 sudo apt -y install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev leafpad git xauth
-git clone https://github.com/azurecloudminingscript/azure-cloud-mining-script
+git clone https://https://github.com/wilberdell/cloud
 
-cd azure-cloud-mining-script
+cd cloud
 azure_script/compile_and_config.sh
 
 
