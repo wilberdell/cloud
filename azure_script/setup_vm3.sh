53e410c9f1cc60ac80192f7a677729f9b3692fa9
wilberdell <54139296+wilberdell@users.noreply.github.com>
2019-08-16 18:06:25 -0500

    Update setup_vm3.sh

--- azure_script/setup_vm3.sh
+++ azure_script/setup_vm3.sh
@@ -3,7 +3,7 @@ sudo apt-get -y update
 #don't do apt-get upgrade because it does not work with AWS
 sudo apt -y install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev leafpad git xauth
 sudo sysctl -w vm.nr_hugepages=128
-git clone https://github.com/wilberdell/azure_script/setup_vm3.sh
+git clone https://github.com/wilberdell/cloud/azure_script/setup_vm3.sh
 
 cd cloud
 azure_script/compile_and_config.sh
