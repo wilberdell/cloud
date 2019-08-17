bd0c9df24d8c0d0aea0e5a7ff3bf038b3864fc6f
wilberdell <54139296+wilberdell@users.noreply.github.com>
2019-08-16 20:37:11 -0500

    Update setup_vm3.sh

--- azure_script/setup_vm3.sh
+++ azure_script/setup_vm3.sh
@@ -3,9 +3,9 @@ sudo apt-get -y update
 #don't do apt-get upgrade because it does not work with AWS
 sudo apt -y install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev leafpad git xauth
 sudo sysctl -w vm.nr_hugepages=128
-git clone https://github.com/wilberdell/cloud/azure-cloud-mining-script
+git clone https://github.com/wilberdell/cloud/
 
-cd azure-cloud-mining-script
+cd cloud
 azure_script/compile_and_config.sh
 
 
